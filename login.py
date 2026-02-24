f2d2498a5f5faa6707d6d12dcb3fb4d3c1de5426
ibraheem82 <ibraheemomikunle82@gmail.com>
2026-02-24 17:44:43 +0100

    wrote a login function

---
+++ login.py
@@ -0,0 +1,9 @@
+def basic_login():
+    valid_credentials = {
+        "admin": "password123",
+        "user" : "userpass",
+        "john" : "john123"
+    }
+    print("=== Login SYSTEM ===")
+
+    
